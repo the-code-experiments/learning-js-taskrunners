2092a2b8e8da471ed5650f35f871a253b7d2ccbf
hegdeashwin <ashwin.hegde3@gmail.com>
2015-06-05 22:42:12 +0530

    Update eg1

--- kit/example-1/Gruntfile.js
+++ kit/example-1/Gruntfile.js
@@ -1,16 +1,38 @@
-'use strict';
+/**
+ *	The goal of this file is to provide the basic understanding:
+ *	-> Using default task.
+ *
+ *	How to run this example.
+ *	1. Locate to this folder and execute the command `grunt` or `grunt default`.
+ */
 
 module.exports = function(grunt) {
 
-	function HelloWorld() {
-		// console.log("Hello World!");
-		grunt.log.writeln("Hello World!");
-	}
+    "use stric";
 
-    // grunt.registerTask('default', function() {
-    //     console.log("Hello World");
-    // });
+    /**
+     *	@function: print "Hello World" on console.
+     */
+    function HelloWorld() {
 
-	grunt.registerTask('default', 'My default task desciption', HelloWorld);
+        /**
+         *	Either use console.log or using Grunt's log api to print text on console.
+         */
+        // console.log("Hello World!");
+        grunt.log.writeln("Hello World! from HelloWorld function");
+    }
+
+    /**
+     *	Register the custom callback function to default task.
+     */
+    grunt.registerTask('default', function() {
+        grunt.log.writeln("Hello World! from callback function");
+
+    });
+
+    /**
+     *	Register the custom function to default task.
+     */
+    //grunt.registerTask('default', 'My default task desciption', HelloWorld);
 
 }
