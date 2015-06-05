ba2b40f045834dd387eea9c19fec3ffaa17d3e99
hegdeashwin <ashwin.hegde3@gmail.com>
2015-06-05 23:13:44 +0530

    Updates eg2

--- kit/example-2/Gruntfile.js
+++ kit/example-2/Gruntfile.js
@@ -1,28 +1,48 @@
-'use strict';
+/**
+ *  The goal of this file is to provide the basic understanding:
+ *  -> How to register a custom task with arguments.
+ *
+ *  How to run this example.
+ *  1. Locate to this folder and execute the command `grunt` or `grunt default`.
+ */
 
 module.exports = function(grunt) {
 
+    "use strict";
+
+    /**
+     *  @task: Custom task.
+     *  @params: firstName, lastName
+     */
     grunt.registerTask('myName', 'A task to get the name', function(firstName, lastName) {
 
+        /**
+         *  Check the length of the arguments.
+         */
         if (arguments.length === 0) {
-            grunt.log.writeln("No names");
+            grunt.log.writeln("Unknown");
         } else {
             grunt.log.writeln(this.name + ": " + firstName + " " + lastName);
         }
 
     });
 
-    grunt.registerTask('default', 'myName');
+    /**
+     *  Register the custom task `myName` to default task.
+     *  Execute the task `myName` without arguments.
+     */
+    // grunt.registerTask('default', 'myName');
 
-    // grunt.registerTask('default', 'myName', function() {
+    /**
+     *  Register the custom task `myName` to default task.
+     *  Execute the task `myName` with arguments.
+     */
+    // grunt.registerTask('default', 'myName:Ashwin:Hegde');
 
-    // 	grunt.log.writeln("Default task");
-
-    // 	// grunt.task.run('myName');
-
-    // 	// grunt.task.run(['myName']);
-
-    // 	// grunt.task.run('myName:Ashwin:Hegde');
-    // });
+    grunt.registerTask('default', 'myName', function() {
+        grunt.task.run('myName');
+        grunt.task.run(['myName']);
+        grunt.task.run('myName:Ashwin:Hegde');
+    });
 
 };
