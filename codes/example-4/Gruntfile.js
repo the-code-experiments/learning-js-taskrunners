917e67a9f92e9372ded6789b0d113d727abc2f5f
hegdeashwin <ashwin.hegde3@gmail.com>
2015-06-13 15:33:01 +0530

    Updates readme.md

--- codes/example-4/Gruntfile.js
+++ codes/example-4/Gruntfile.js
@@ -1,6 +1,6 @@
 /**
  *  The goal of this file is to provide the basic understanding:
- *  -> How to configure jshint task (3rd party npm).
+ *  -> How to configure jshint task (3rd party grunt plugin).
  *
  *  How to run this example.
  *  1. Locate to this folder and execute the command `grunt` or `grunt default`.
